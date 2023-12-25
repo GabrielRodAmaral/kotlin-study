a2d78e8f654f852c4cc0034b521f2c6a4f6f8059
Gabriel Amaral <142855392+GabrielRodAmaral@users.noreply.github.com>
2023-12-24 22:22:31 -0300

    Update Main.kt

--- firstKotlinProject/src/main/kotlin/firstSteps/Main.kt
+++ firstKotlinProject/src/main/kotlin/firstSteps/Main.kt
@@ -2,8 +2,6 @@ package firstSteps
 
 import variables.exerciseTwoVariables
 
-// Nessa main estou chamando funções de todos os packages e falando o que a função chamada explica para fácil
-// visualização do conteúdo
 fun main() {
     println("Hello Kotlin!")
 }
